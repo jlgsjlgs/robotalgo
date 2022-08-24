68d5aa35bcedb88aed9194f38b173dc9f929dfd6
Jonathan Lim <jonathanlimgs@hotmail.com>
2022-08-24 23:10:55 +0800

    Fixed comment issue

--- algorithm.py
+++ algorithm.py
@@ -69,6 +69,7 @@ def exhaustiveSearch(obslist):
     print("Lowest cost found = {}, with path as {}".format(lowestcost,lowestcostpath))
 
 # def astarsearch(obslist):
+    # exhaustiveSearch(obslist)
 
 
         
